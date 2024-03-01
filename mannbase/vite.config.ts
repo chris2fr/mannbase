2cc4b82614f649944dbbf22683775b52e6bb9b0d
Chris Mann <chris@lesgrandsvoisins.com>
2024-03-01 05:43:28 +0100

    Still compiles

--- mannbase/vite.config.ts
+++ mannbase/vite.config.ts
@@ -1,6 +1,27 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import type { UserConfig } from "vite";
+import fs from "fs";
+import path from "path";
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+// detect if we're running inside docker and set the backend accordingly
+const pocketbase_url = fs.existsSync("/.dockerenv")
+  ? "http://pb:8090" // docker-to-docker
+  : "http://localhost:8090"; // localhost-to-localhost
+
+const config: UserConfig = {
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      // proxy "/api" and "/_" to pocketbase_url
+      "/api": pocketbase_url,
+      "/_": pocketbase_url,
+    },
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
+};
+
+export default config;
