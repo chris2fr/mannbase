8db49e5fbe61e1d30d4eb6f3f06418b913a09a89
Chris Mann <chris@lesgrandsvoisins.com>
2024-03-01 05:49:12 +0100

    Still compiles

---
+++ mannbase/src/lib/pocketbase/generated-types.ts
@@ -0,0 +1,125 @@
+/**
+* This file was @generated using pocketbase-typegen
+*/
+
+import type PocketBase from 'pocketbase'
+import type { RecordService } from 'pocketbase'
+
+export enum Collections {
+	Auditlog = "auditlog",
+	Consultations = "consultations",
+	Hooks = "hooks",
+	Posts = "posts",
+	Users = "users",
+}
+
+// Alias types for improved usability
+export type IsoDateString = string
+export type RecordIdString = string
+export type HTMLString = string
+
+// System fields
+export type BaseSystemFields<T = never> = {
+	id: RecordIdString
+	created: IsoDateString
+	updated: IsoDateString
+	collectionId: string
+	collectionName: Collections
+	expand?: T
+}
+
+export type AuthSystemFields<T = never> = {
+	email: string
+	emailVisibility: boolean
+	username: string
+	verified: boolean
+} & BaseSystemFields<T>
+
+// Record types for each collection
+
+export type AuditlogRecord<Tdata = unknown, Toriginal = unknown> = {
+	admin?: string
+	collection: string
+	data?: null | Tdata
+	event: string
+	original?: null | Toriginal
+	record: string
+	user?: RecordIdString
+}
+
+export type ConsultationsRecord = {
+	notes?: HTMLString
+	quand?: IsoDateString
+	user?: RecordIdString
+}
+
+export enum HooksEventOptions {
+	"insert" = "insert",
+	"update" = "update",
+	"delete" = "delete",
+}
+
+export enum HooksActionTypeOptions {
+	"command" = "command",
+	"email" = "email",
+	"post" = "post",
+}
+export type HooksRecord = {
+	action: string
+	action_params?: string
+	action_type: HooksActionTypeOptions
+	collection: string
+	disabled?: boolean
+	event: HooksEventOptions
+	expands?: string
+}
+
+export type PostsRecord = {
+	body: string
+	files?: string[]
+	slug: string
+	title: string
+	user?: RecordIdString
+}
+
+export type UsersRecord = {
+	avatar?: string
+	consultations?: RecordIdString[]
+	name?: string
+}
+
+// Response types include system fields and match responses from the PocketBase API
+export type AuditlogResponse<Tdata = unknown, Toriginal = unknown, Texpand = unknown> = Required<AuditlogRecord<Tdata, Toriginal>> & BaseSystemFields<Texpand>
+export type ConsultationsResponse<Texpand = unknown> = Required<ConsultationsRecord> & BaseSystemFields<Texpand>
+export type HooksResponse<Texpand = unknown> = Required<HooksRecord> & BaseSystemFields<Texpand>
+export type PostsResponse<Texpand = unknown> = Required<PostsRecord> & BaseSystemFields<Texpand>
+export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
+
+// Types containing all Records and Responses, useful for creating typing helper functions
+
+export type CollectionRecords = {
+	auditlog: AuditlogRecord
+	consultations: ConsultationsRecord
+	hooks: HooksRecord
+	posts: PostsRecord
+	users: UsersRecord
+}
+
+export type CollectionResponses = {
+	auditlog: AuditlogResponse
+	consultations: ConsultationsResponse
+	hooks: HooksResponse
+	posts: PostsResponse
+	users: UsersResponse
+}
+
+// Type for usage with type asserted PocketBase instance
+// https://github.com/pocketbase/js-sdk#specify-typescript-definitions
+
+export type TypedPocketBase = PocketBase & {
+	collection(idOrName: 'auditlog'): RecordService<AuditlogResponse>
+	collection(idOrName: 'consultations'): RecordService<ConsultationsResponse>
+	collection(idOrName: 'hooks'): RecordService<HooksResponse>
+	collection(idOrName: 'posts'): RecordService<PostsResponse>
+	collection(idOrName: 'users'): RecordService<UsersResponse>
+}
